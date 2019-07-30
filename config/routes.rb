1388a9f87aacd58914641858d9f847230e069620
Jordan Fasso <jordan.fasso@gmail.com>
2019-07-30 16:10:05 +1000

    stripe included and join table for profile complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :flyers
+  resources :flyers do
+    resources :charges
+  end
   resources :profiles
+  
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
