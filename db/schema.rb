379a38dd29a5d19296173e28f5193259b3e337f6
PushBtnsDev <kyledchenoweth@gmail.com>
2019-08-06 11:56:38 +1000

    Added image_link column to category in database, updated seed file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_29_061351) do
+ActiveRecord::Schema.define(version: 2019_08_06_013223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2019_07_29_061351) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_link"
   end
 
   create_table "flyer_attended_by_users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,48 +10,74 @@
 # categories = [
 #   {
 #     name: "Music",
+#     image_link: "https://dl.dropboxusercontent.com/s/cu6nxsy712sb25h/music.jpg?dl=0"
 #   },
 #   {
 #     name: "Tech",
+#     image_link: "https://dl.dropbox.com/s/skr8i19h1fo27rw/tech.jpg?dl=0"
 #   },
 #   {
 #     name: "Cooking",
+#     image_link: "https://dl.dropbox.com/s/ahhgnvyym6w8hoa/cooking.jpg?dl=0"
 #   },
 #   {
 #     name: "Life Skills",
+#     image_link: "https://dl.dropbox.com/s/94ijuefujexwius/life-skill.jpg?dl=0"
 #   },
 #   {
 #     name: "Tutoring",
+#     image_link: "https://dl.dropbox.com/s/pd7dplj7rk89zw0/tutoring.jpg?dl=0"
 #   },
 #   {
 #     name: "Business",
+#     image_link: "https://dl.dropbox.com/s/fos3145gjeg67i6/business.jpg?dl=0"
 #   },
 #   {
 #     name: "Office Skills",
+#     image_link: "https://dl.dropbox.com/s/84gazs1vzkui1ga/office-skills.jpg?dl=0"
 #   },
 #   {
 #     name: "Languages",
+#     image_link: "https://dl.dropbox.com/s/v55bejioxoc9l11/languages.jpg?dl=0"
 #   },
 #   {
 #     name: "Personal Development",
+#     image_link: "https://dl.dropbox.com/s/pn1ofsp1g9iqgdh/personal-development.jpg?dl=0"
 #   },
 #   {
 #     name: "Arts",
+#     image_link: "https://dl.dropbox.com/s/skjbth47ebovsfw/art.jpg?dl=0"
 #   },
 #   {
 #     name: "Photography",
+#     image_link: "https://dl.dropbox.com/s/y58v42ma4g9vh1n/photography.jpg?dl=0"
 #   },
 #   {
 #     name: "Fitness",
+#     image_link: "https://dl.dropbox.com/s/ba535wbra2l6h92/fitness.jpg?dl=0"
 #   },
 #   {
 #     name: "Academics",
+#     image_link: "https://dl.dropbox.com/s/8zfb5fo0azskclm/academics.jpg?dl=0"
 #   },
 #   {
 #     name: "Other",
+#     image_link: "https://dl.dropbox.com/s/67wfk94m2ec0k73/other.png?dl=0"
 #   },
 # ]
 
+
+#### ONLY USE IF HELD PREVIOUS DATABASE OF ONLY CATEGORY NAMES
+# count_id = 1
+# categories.each do |category|
+#   database_category = Category.find(count_id)
+#   database_category.image_link = category[:image_link]
+#   database_category.save!
+#   count_id += 1
+# end
+#### 
+
+#### INITIALISE CATEGORIES
 # categories.each do |category|
 #   Category.create!(category)
 # end
