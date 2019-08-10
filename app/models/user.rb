c76363da5d2a3ebbd0b7ebf520db6045ab293892
Jordan Fasso <jordan.fasso@gmail.com>
2019-08-10 13:16:12 +1000

    updated flyers, home, projects fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :flyers_attended_by_users
+  has_many :flyers_attended_by_users 
   has_many :flyers, through: :flyers_attended_by_users
   has_one :profile
 end
